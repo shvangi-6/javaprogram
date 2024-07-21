a2c0e6840afe9ddb768b83dd619d767782f42a1c
Shivangi chaubey <118792511+shvangi-6@users.noreply.github.com>
2024-07-21 17:17:04 +0530

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Name {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String Name=sc.nextLine();
+        System.out.println("GOOD MORNIG");
+    }
+    
+}
